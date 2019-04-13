6138a9a92e0bdfddecd4ddfacd823961c8436404
Johan Lindeborg <johanlindeborg248@gmail.com>
2019-04-13 18:24:28 +0200

    Börjat på server delen

---
+++ server/server/Server.java
@@ -0,0 +1,59 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Server {
+	
+	//Hashmap to store usernames (key) and their associated threads.
+	private HashMap<String, WorkerThread> clientMap;
+	
+	
+	private ConnectionAccepter connectionAccepter;
+		
+	public Server() {
+		
+		
+		connectionAccepter = new ConnectionAccepter();
+		
+		connectionAccepter.start();
+	}
+	
+	
+	private class ConnectionAccepter extends Thread{
+		private ServerSocket serverSocket;
+		private int serverPort = 4242;
+		
+		public ConnectionAccepter() {
+			
+			try {
+				serverSocket = new ServerSocket(serverPort);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			
+		}
+		
+		public void run() {
+			while(this.isAlive()){
+		       
+				Socket clientSocket = null;
+		        try {
+		            clientSocket = this.serverSocket.accept();
+		        } catch (IOException e) {
+		            if(this.isAlive() != true) {
+		                System.out.println("Server Stopped.") ;
+		                return;
+		            }
+		            throw new RuntimeException(
+		                "Error accepting client connection", e);
+		        }
+		        new WorkerThread(clientSocket).start();
+		    }
+		}
+	}
+
+}
---
+++ server/server/WorkerThread.java
@@ -0,0 +1,42 @@
+package server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class WorkerThread extends Thread {
+
+	private Socket clientSocket;
+	
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+	
+	
+	public WorkerThread(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+		
+		try {
+			this.ois = new ObjectInputStream(clientSocket.getInputStream());    
+			this.oos = new ObjectOutputStream(clientSocket.getOutputStream());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void run() {
+        
+		while(this.isAlive()) {
+			try {
+	            
+	        	
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+    }
+		
+	
+}
