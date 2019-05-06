fb1f902b933b3060bd3969fc8cbd2e32b25fd89b
dame910517 <45669889+dame910517@users.noreply.github.com>
2019-05-06 14:50:43 +0200

    Update GameMenu.java

--- src/gui/GameMenu.java
+++ src/gui/GameMenu.java
@@ -1,32 +1,36 @@
-package gui;
+package client;
 
+import java.awt.BorderLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 import shared.User;
 
 public class GameMenu extends JPanel implements ActionListener {
-	private JButton btnGameRules = new JButton("Game rules");// Show game rules
 	private JButton btnStart = new JButton("Start");// Start game
-	private ClientGUI gui = new ClientGUI();
+	private Image image;
 	// Alternatives for choosing map and gametype
 	private JLabel lblMap; // Choose map
 	private JLabel lblGameType; // Choose gametype
-	private String[] options = { "Africa", "etc" };
+	private String[] options = { "Choose country", "France","Sweden","Italy","Germany", "Greece" };
 	private JComboBox<String> cmbChooseMap = new JComboBox<String>(options); // Choose map combo box
 //	private JComboBox cmbGameType; // Choose game type combo box
 	private LinkedList<User> usersOnline = new LinkedList<User>();
 	private JComboBox<User> cmbChooseUser = new JComboBox<User>();
 
 	public GameMenu() {
-
-		btnGameRules.setBounds(110, 300, 120, 23);
-		add(btnGameRules);
 		btnStart.setBounds(380, 300, 120, 23);
 		add(btnStart);
 
@@ -35,15 +39,33 @@ public class GameMenu extends JPanel implements ActionListener {
 		cmbChooseUser.setBounds(380, 100, 120, 23);
 		add(cmbChooseUser);
 
-		btnGameRules.addActionListener(this);
 		btnStart.addActionListener(this);
 		cmbChooseMap.addActionListener(this);
 		cmbChooseUser.addActionListener(this);
+		try {
+			image = ImageIO.read(new File("images/worldmap2.jpg"));
+			JLabel label = new JLabel(new ImageIcon(image));
+			this.add(label);
+
+		} catch (IOException ex) {
+			System.out.print("Image exception" + ex);
+		}
+	}
+
+	void showUI() {
+		JFrame frame = new JFrame("GeoGo-mapLocator");
+//		setUserMenu();
+//      setUserList();
+		frame.setLayout(new BorderLayout());
+		frame.add(this, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
