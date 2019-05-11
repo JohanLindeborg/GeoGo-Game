42bc2eb35c00b6c887d90c386ce1d4f62849be5b
Andreas Holm <andreasamholm@gmail.com>
2019-05-12 00:56:30 +0200

    Fixade pos knappar i StartMenu fönster

--- src/client/StartMenu.java
+++ src/client/StartMenu.java
@@ -1,4 +1,3 @@
-
 package client;
 
 import java.awt.BorderLayout;
@@ -25,70 +24,74 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
 public class StartMenu extends JPanel implements ActionListener {
 	private JButton bnSingle = new JButton("Singleplayer");
 	private JButton bnMulti = new JButton("Multiplayer");
 	private JButton bnRules = new JButton("Game Instructions");
+	private JButton exitBtn = new JButton("Exit Game");
 	private Image image;
 	private GraphicsEnvironment ge;
 	private JFrame frame;
-	
-	private JButton goBackBtn = new JButton("Exit Game");
+	private CardLayout cl;
 
-	
 	public StartMenu() {
+
 		ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		Rectangle bounds = ge.getMaximumWindowBounds();
-		
-		//this.setLayout(new BorderLayout());
-		this.setPreferredSize(new Dimension(bounds.width, bounds.height)); // The games outer panel
 
+		// this.setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(bounds.width, bounds.height)); // The games outer panel
+
+		bnSingle.setBounds((bounds.width / 2) - 70, (bounds.height / 2) - 80, 140, 40);
+		bnMulti.setBounds((bounds.width / 2) - 70, (bounds.height / 2) - 20, 140, 40);
+		bnRules.setBounds((bounds.width / 2) - 70, (bounds.height / 2) + 40, 140, 40);
+		exitBtn.setBounds((bounds.width / 2) - 70, (bounds.height / 2) + 100, 140, 40);
+		
 		// Add buttons to this panel
-		bnSingle.setBounds(bounds.width/2, bounds.height/2, 130, 30);// GridbagLayout, dynamiska layouter
 		add(bnSingle);
-		bnMulti.setBounds(bounds.width/2, (bounds.height/2)+80, 130, 30);
 		add(bnMulti);
-		bnRules.setBounds(bounds.width/2, (bounds.height/2)+160, 130, 30);
 		add(bnRules);
-		add(goBackBtn);
+		add(exitBtn); // exit button
 
 		// Add actionelisteners
 		bnSingle.addActionListener(this);
 		bnMulti.addActionListener(this);
 		bnRules.addActionListener(this);
-		goBackBtn.addActionListener(this);
+		exitBtn.addActionListener(this);
 		
+		this.setLayout(cl); // Put the cardlayout on the panel
+//		cards = new JPanel(new CardLayout()); // Create the panel(parent?) that contains the "cards".
+//		cl = (CardLayout) (cards.getLayout());// Nödvändig? Ja annars nullpointer, måste ha getlayout
+//		cards.add(startMenu, STARTGAMEPANEL); // lägger denna till startmenu med namnet start
+		
+
 		BufferedImage image = null;
 		try {
-		    image = ImageIO.read(new File("images/world.jpg"));
+			image = ImageIO.read(new File("images/world.jpg"));
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		
+
 		JLabel imageLbl = new JLabel();
 		imageLbl.setBounds(0, 0, bounds.width, bounds.height);
-		
-		Image dimg = image.getScaledInstance(imageLbl.getWidth(), imageLbl.getHeight(),Image.SCALE_SMOOTH);
-		
+
+		Image dimg = image.getScaledInstance(imageLbl.getWidth(), imageLbl.getHeight(), Image.SCALE_SMOOTH);
+
 		imageLbl.setIcon((new ImageIcon(dimg)));
 		this.add(imageLbl);
+		
 		/*
-		try {
-			image = ImageIO.read(new File("images/worldmap2.jpg"));
-			
-			JLabel label = new JLabel(new ImageIcon(image));
-			this.add(label);
-			label.setBounds(0, 0, bounds.width, bounds.height);
-
-		} catch (IOException ex) {
-			System.out.print("Image exception" + ex);
-		}
-		*/
+		 * try { image = ImageIO.read(new File("images/worldmap2.jpg"));
+		 * 
+		 * JLabel label = new JLabel(new ImageIcon(image)); this.add(label);
+		 * label.setBounds(0, 0, bounds.width, bounds.height);
+		 * 
+		 * } catch (IOException ex) { System.out.print("Image exception" + ex); }
+		 */
 	}
 
 	void showUI() {
-		frame = new JFrame("GeoGo-mapLocator");
+		frame = new JFrame("StartMenu");
 //			setUserMenu();
 //	      setUserList();
 		frame.setLayout(new BorderLayout());
@@ -101,13 +104,12 @@ public class StartMenu extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Object input = e.getSource();
 		if (input.equals(bnSingle)) {
-				singlePlayer();
+			singlePlayer();
 		} else if (input.equals(bnMulti)) {
 //				multiPlayer();
 		} else if (input.equals(bnRules)) {
 			gameRules();
-		}
-		else if(input.equals(goBackBtn)) {
+		} else if (input.equals(exitBtn)) {
 			frame.dispose();
 		}
 	}
@@ -131,8 +133,10 @@ public class StartMenu extends JPanel implements ActionListener {
 	}
 
 	public void gameRules() {
-		JOptionPane.showMessageDialog(null, "The task of a player of GeoGo is to estimate the locations of different cities on a worldmap. "+ "\n" 
-								+ "A score is then given the player as a result of the difference in distance between the estimated spot and the actual location."+"\n" + 
-								"Players can choose what part of the world him or her wishes to play in."+"\n"+"\n"+"Upgrade your geographical knowledge now!");
+		JOptionPane.showMessageDialog(null,
+				"The task of a player of GeoGo is to estimate the locations of different cities on a worldmap. " + "\n"
+						+ "A score is then given the player as a result of the difference in distance between the estimated spot and the actual location."
+						+ "\n" + "Players can choose what part of the world him or her wishes to play in." + "\n" + "\n"
+						+ "Upgrade your geographical knowledge now!");
 	}
 }
