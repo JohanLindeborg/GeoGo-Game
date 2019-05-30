d68cf822727a6758fdb8a0fbaf90fd560494053b
Johan Lindeborg <johanlindeborg248@gmail.com>
2019-05-30 11:26:25 +0200

    fix

--- src/sharedFiles/RequestGameMsg.java
+++ src/sharedFiles/RequestGameMsg.java
@@ -11,7 +11,7 @@ import java.awt.geom.Point2D;
 public class RequestGameMsg extends Message {
 	private static final long serialVersionUID = 1L;
 	
-	String player2;
+	private String player2;
 	private Point2D.Double mapCenter;
 	private double zoomLevel;
 	private String mapName;
