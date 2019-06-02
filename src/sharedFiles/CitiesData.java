1960a758921511fa41fa29491e7be76b3f6ad52a
dame910517 <45669889+dame910517@users.noreply.github.com>
2019-06-02 13:57:05 +0200

    Update CitiesData.java

--- src/sharedFiles/CitiesData.java
+++ src/sharedFiles/CitiesData.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * reads from a file depending on which map has been selected by the user.
  * @author johanlindeborg
  * @author Adam
+ * @author Amanda Eriksson
  */
 public class CitiesData {
 	private LinkedList<City> mapCities = new LinkedList<City>();
